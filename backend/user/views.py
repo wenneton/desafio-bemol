3f37b551041716c8b449ba9a68e11ee8ff78db5f
Marcos Wenneton Vieira de Araujo <marcosw@motorola.com>
2021-01-26 16:00:53 -0400

    create crud endpoints for user

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,17 +1,57 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+
+from user.models import User
+from user.serializers import UserSerializer
 
 
 @api_view(['GET', 'POST', 'DELETE'])
 def users_list(request):
-    pass
+    if request.method == 'GET':
+        users = User.objects.all()
+        
+        name = request.GET.get('name', None)
+        if name is not None:
+            users = users.filter(name__icontains=name)
+        
+        user_serializer = UserSerializer(users, many=True)
+        return JsonResponse(user_serializer.data, safe=False)
+        # 'safe=False' for objects serialization
+    elif request.method == 'POST':
+        user_data = JSONParser().parse(request)
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse(user_serializer.data, status=status.HTTP_201_CREATED) 
+        return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        count = User.objects.all().delete()
+        return JsonResponse({'message': '{} users were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_detail(request, pk):
-    # find user by id
+    # find user by primary key (id)
     try: 
         user = User.objects.get(pk=pk) 
     except User.DoesNotExist: 
-        return JsonResponse({'message': 'The user does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+        return JsonResponse({'message': 'The user does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET': 
+        user_serializer = UserSerializer(user) 
+        return JsonResponse(user_serializer.data)
+    elif request.method == 'PUT': 
+        user_data = JSONParser().parse(request) 
+        user_serializer = UserSerializer(user, data=user_data) 
+        if user_serializer.is_valid(): 
+            user_serializer.save() 
+            return JsonResponse(user_serializer.data) 
+        return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE': 
+        user.delete() 
+        return JsonResponse({'message': 'User was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT) 
  
     # GET / PUT / DELETE user
